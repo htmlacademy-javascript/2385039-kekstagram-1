e30d9e82924801280ba441e5e3eb32cbaa539508
Keks <keks@htmlacademy.ru>
2024-05-19 20:02:18 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -1,42 +1,81 @@
 // Задание 1
-function isPalindrome(str) {
-  const newStr = str.split('').reverse().join('');
-  if (str.replaceAll(' ', '').toLowerCase() === newStr.replaceAll(' ', '').toLowerCase()) {
-    return console.log('Задание 1: true');
-  } else {
-    return console.log('Задание 1: false');
+const isPalindrome = (string) => {
+  const newStr = string
+    .split('')
+    .reverse()
+    .join('')
+    .replaceAll(' ', '')
+    .toLowerCase();
+  if (string.replaceAll(' ', '').toLowerCase() === newStr) {
+    return true;
   }
-}
-isPalindrome('топот');
-isPalindrome('ДовОд');
-isPalindrome('Кекс');
-isPalindrome('Лёша на полке клопа нашёл ');
+  return false;
+};
+console.log(isPalindrome('топот'));
+console.log(isPalindrome('ДовОд'));
+console.log(isPalindrome('Кекс'));
+console.log(isPalindrome('Лёша на полке клопа нашёл '));
+
+// Задание 1 , другой вариант решения
+const isPalindrome1 = (string) => {
+  const interimString = string.replaceAll(' ', '').toLowerCase();
+  let reverseString = '';
+  for (let i = interimString.length - 1; i >= 0; i--) {
+    reverseString += interimString.at(i);
+  }
+  return interimString === reverseString;
+};
+console.log(isPalindrome1('топот'));
+console.log(isPalindrome1('ДовОд'));
+console.log(isPalindrome1('Кекс'));
+console.log(isPalindrome1('Лёша на полке клопа нашёл '));
 
 // Задание 2
-function isNumber (str) {
-  const newsrt = str.replace(/[^0-9]/g, '');
-  if (newsrt) {
-    return console.log(newsrt);
-  } else {
-    return console.log(NaN);
+const isNumber1 = (string) => {
+  if (typeof string === 'number') {
+    return string;
   }
-}
-isNumber('2023 год');
-isNumber('ECMAScript 2022');
-isNumber('1 кефир, 0.5 батона');
-isNumber('агент 007');
-isNumber('а я томат');
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
+  }
+  return parseInt(result, 10);
+};
+console.log(isNumber1('2023 год'));
+console.log(isNumber1('ECMAScript 2022'));
+console.log(isNumber1('1 кефир, 0.5 батона'));
+console.log(isNumber1('агент 007'));
+console.log(isNumber1('а я томат'));
+console.log(isNumber1(2023));
+console.log(isNumber1(-1));
+console.log(isNumber1('1.5'));
+console.log(isNumber1(-1.5));
 
+// Задание 3
+const myPadStart = (string, minLength, pad) => {
+  const actualPad = minLength - string.length;
+  if (actualPad <= 0) {
+    return string;
+  }
+  const tempPad = pad.slice(0, actualPad % pad.length);
+  const tempRepeat = pad.repeat(actualPad / pad.length);
+  return tempPad + tempRepeat + string;
+};
+console.log(myPadStart('1', 2, '0'));
+console.log(myPadStart('1', 4, '0'));
+console.log(myPadStart('q', 4, 'werty'));
+console.log(myPadStart('q', 4, 'we'));
+console.log(myPadStart('qwerty', 4, '0'));
 
 // Задание 4
-function compare(str, maxLength) {
-  if (str.length <= maxLength) {
-    return console.log('Задание 4: true');
-  } else {
-    return console.log('Задание 4: false');
+const isCompare = (string, maxLength) => {
+  if (string.length <= maxLength) {
+    return true;
   }
-}
-compare('проверяемая строка', 20);
-compare('проверяемая строка', 18);
-compare('проверяемая строка', 10);
-
+  return false;
+};
+console.log(isCompare('проверяемая строка', 20));
+console.log(isCompare('проверяемая строка', 18));
+console.log(isCompare('проверяемая строка', 10));
