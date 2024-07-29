508897ff70f7116732a93bebb9a4f80e2c7b8922
Гимранова Динара Масгутовна <GimranovaDM@RIV.AMETIST.PRO>
2024-07-29 12:21:27 +0300

    создает валидацию формы

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ import { renderPictures } from './picture-render.js';
 import { initPicturePreview } from './gallery.js';
 import './upload-form.js';
 
-
 const data = createContent();
 renderPictures(data);
 initPicturePreview(data);
--- js/scale.js
+++ js/scale.js
@@ -1,4 +1,4 @@
-const scaleControlValue = document.querySelector('.scale__control--value');
+/*const scaleControlValue = document.querySelector('.scale__control--value');
 const buttonScaleSmaller = document.querySelector('.scale__control--smaller');
 const buttonScaleBigger = document.querySelector('.scale__control--bigger');
 const pictureUploadPreview = document.querySelector('.img-upload__preview img');
@@ -6,6 +6,4 @@ const pictureUploadPreview = document.querySelector('.img-upload__preview img');
 
 const MIN_SCALE_VALUE = 25;
 const MAX_SCALE_VALUE = 100;
-const DEFAULT_SCALE_STEP = 25;
-
-
+const DEFAULT_SCALE_STEP = 25;*/
