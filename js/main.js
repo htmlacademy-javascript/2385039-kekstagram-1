ac2d1d9c1952ba58db2bdd55cc08a9b21d0b27ca
Гимранова Динара Масгутовна <GimranovaDM@RIV.AMETIST.PRO>
2024-07-31 12:05:56 +0300

    выполняет редактирование масштаба картинки

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ import { createContent } from './data.js';
 import { renderPictures } from './picture-render.js';
 import { initPicturePreview } from './gallery.js';
 import { setupValidation } from './upload-form.js';
+import { resetScaleValue } from './scale.js';
 
 const data = createContent();
 renderPictures(data);
 initPicturePreview(data);
 setupValidation();
+resetScaleValue();
